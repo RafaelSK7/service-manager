0a780f6579ea5820d22c0e795a8ec6d4b742fa60
Rafael Kiss <69167382+RafaelSK7@users.noreply.github.com>
2023-08-18 10:19:45 -0300

    Add files via upload

---
+++ src/test/java/rafael/kiss/servicemanager/ServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package rafael.kiss.servicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
